77a8c0a01639c6c6294b71518bf808531fa8771f
Daichi Fukuoka <daichi.fukuoka@hpe.com>
2017-10-17 15:35:08 +0900

    update test.py

--- test.py
+++ test.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 
-import os, sys
+import sys
 import numpy as np
-from numpy.random import *
 import mydgemm as md
-from mydgemm2 import *
+import mydgemm2 as md2
 import time as T
 
 def main():
     argv = sys.argv
     argc = len(argv)
     s = 7777
-    seed(s)
+    np.random.seed(s)
     if argc == 1:
         size = 128
     else:
         size = int(argv[1])
     C = np.zeros((size, size), dtype=np.float64)
-    A = np.array(rand(size, size), dtype=np.float64)
-    B = np.array(rand(size, size), dtype=np.float64)
+    A = np.array(np.random.rand(size, size), dtype=np.float64)
+    B = np.array(np.random.rand(size, size), dtype=np.float64)
 
     print("matrix size:", size, "x", size)
     
@@ -26,21 +25,21 @@ def main():
     C = md.mydgemm(A, B)
     time = T.time() - t0
     np.save("mydgemm.out", C)
-    print("time for mydgemm()[s]:", time)
+    print("time of mydgemm()[s]:", time)
     
     C = 0.0
     t0 = T.time()
-    C = mydgemm2(A, B)
+    C = md2.mydgemm2(A, B)
     time = T.time() - t0
     np.save("mydgemm2.out", C)
-    print("time for mydgemm2()[s]:", time)
+    print("time of mydgemm2()[s]:", time)
 
     C = 0.0
     t0 = T.time()
     C = np.dot(A, B)
     time = T.time() - t0
     np.save("np.dot.out", C)
-    print("time for np.dot()[s]:", time)
+    print("time of np.dot()[s]:", time)
     
 if __name__ == "__main__":
     main()
